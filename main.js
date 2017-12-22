8e151a099413a41861008e8bb5001b58d5d199a8
ivonka0225 <ivona1234is@gmail.com>
2017-12-22 18:14:21 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    
+    $('#button').click( function(){ 
+	var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) { 
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800); 
+        }
+	    return false; 
+    });
+    
+    
+});
